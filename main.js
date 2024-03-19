7525bcd632c35cf53be4ed9880c26ccaae11fa46
Rashad Aliyev <eliyevresad901@gmail.com>
2024-03-20 01:57:36 +0400

    sample js tasks

---
+++ main.js
@@ -0,0 +1,140 @@
+// task-01
+// console.log("My name is Rashad");
+
+// task-02
+// const myName = "Rashad";
+// console.log(myName);
+
+// task-03
+// personalbar,$add
+
+/*task-04
+let bookPrice = 750;
+let bookAmount = 14;
+
+let totalAmount = bookAmount * bookPrice ;
+console.log(totalAmount);*/
+
+/*task-05
+let kmhSpeed = 75;
+let msSpeed = kmhSpeed *1000 / 3600;
+console.log(msSpeed);*/
+
+/* task-06
+let birthday = Number(prompt("what is your birthday"))
+let thisday = 2024
+let totalDays =(thisday - birthday)*365
+console.log(totalDays);*/
+
+/*task-07
+let num = 58;
+let besinciDerece =Math.pow(num,5);
+let qaliq = num % 3;
+console.log(`${num} ededin besinci derece quvveti:${besinciDerece}`);
+console.log(`${num} ededin 3 e bolunmeden alinan qaligi: ${qaliq}`);*/
+
+/* task-08
+let firstName = "Rashad";
+let lastName = "Aliyev";
+console.log(`Sizin adiniz ${firstName} ${lastName} .Tanisligimiza sadam!`);*/
+
+/* task-09
+let firstName = prompt("Adinizi daxil edin")
+let lastName = prompt("Soyadinizi daxil edin")
+console.log(`We are glad to see you again:${firstName} ${lastName}`);*/
+
+/*let a = 3
+let b = a++ - --a
+let c = b-- - --a + ++b
+let d = c++ + a-- - b--
+
+console.log(a,b,c,d);*/
+
+/*task-10
+let fullName = "Rashad Aliyev";
+let letterCount = 0;
+
+for (let i = 0; i < fullName.length; i++) {
+    if (fullName[i] !== ' ') {
+        letterCount++;
+    }
+}
+
+console.log("Addaki hərflərin sayı: " + letterCount);*/
+
+/*  task-11
+let fullName = "Murad Orucov";
+let names = fullName.split(" ");
+
+if (names.length >= 2) {
+    let firstInitial = names[0][0];
+    let lastInitial = names[1][0];
+    console.log("İstifadəçinin ad soyadının baş hərfləri:", firstInitial, lastInitial);
+} else {
+    console.log("Ad və soyadı ayırmaq üçün boşluq istifadə edin.");
+}*/
+
+/* task-12
+const fullName = prompt("Zəhmət olmasa tam adınızı daxil edin (AD SOYAD formatında):"); 
+
+const splitName = fullName.split(" "); 
+const reversedName = splitName[1] + " " + splitName[0]; 
+
+console.log(reversedName);*/
+
+/*task-13
+const name = prompt("Zəhmət olmasa adınızı daxil edin:"); 
+
+console.log(`Salam, ${name}!`); */
+
+/*task-14
+const number1 = +prompt("Zəhmət olmasa birinci ədədi daxil edin:");
+const number2 = +prompt("Zəhmət olmasa ikinci ədədi daxil edin:");
+
+// Toplama əməliyyatı
+const sumResult = number1 + number2;
+console.log("Toplama nəticəsi:", sumResult);
+
+// Çıxma əməliyyatı
+const subtractionResult = number1 - number2;
+console.log("Çıxma nəticəsi:", subtractionResult);
+
+// Vurma əməliyyatı
+const multiplicationResult = number1 * number2;
+console.log("Vurma nəticəsi:", multiplicationResult);
+
+// Bölmə əməliyyatı
+const divisionResult = number1 / number2;
+console.log("Bölmə nəticəsi:", divisionResult);*/
+
+
+/*task-15
+let num1 = 12345;
+let num2 =  98765;
+let total = String(num1*num2)
+console.log(total);
+console.log(total[5]);*/
+
+/* task-18
+let ad = prompt("Adınızı daxil edin:");
+let soyad = prompt("Soyadınızı daxil edin:");
+
+console.log("Salam, " + ad + "!");*/
+
+/* task-23
+value === 10 (true)
+value !== 20 (true)
+value < 15 (true)
+value >= 10 (true)
+value !== 20 && value === 10 (true)*/
+
+/* task-24
+let yas = parseInt(prompt("Yaşınızı daxil edin:"));
+
+if (yas < 18) {
+    alert("Access denied");
+} else {
+    alert("Access granted");
+}*/
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+num1=123456
+num2=678910
+total=str(num1*num2)
+print(total[5])
